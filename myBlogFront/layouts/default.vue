52dfcb7d13c8be02278217ea0e249e8fe322fafb
翟海祥 <zhaihaixiang@u-care.net.cn>
2020-02-26 10:35:29 +0800

    初始化

---
+++ myBlogFront/layouts/default.vue
@@ -0,0 +1,17 @@
+<!--
+ * @Author: 翟海祥
+ * @Date: 2020-02-26 10:32:00
+ * @LastEditTime: 2020-02-26 10:35:06
+ * @LastEditors: 翟海祥
+ * @Description:
+ * @FilePath: \myBlogFront\layouts\default.vue
+ -->
+<template>
+  <div>
+    <nuxt />
+  </div>
+</template>
+
+<style>
+
+</style>
